17378f7acb5ee6b58ff87cab1b0e441018b6f7c6
smogm <you@example.com>
2019-02-20 21:25:43 +0100

    add debug to rpc

--- controllers/rpc.js
+++ controllers/rpc.js
@@ -6,10 +6,15 @@ exports.sendrawtransaction = (req, res) => {
   const raw = req.body.raw;
   const allowhighfees = stringToBoolean(req.body.allowhighfees);
 
+  console.log(raw);
+  console.log(allowhighfees);
   if (!raw) return res.status(400).send({ result: 'error', message: 'Empty raw data!' });
 
   try {
+    console.log("calling wallet");
     return client.call('sendrawtransaction', [raw, allowhighfees], (err, result) => {
+      console.log(err);
+      console.log(result);
       if (err) return res.status(400).send({ result: 'error', message: 'Error occured while sending raw transaction', error: err.error });
       return res.status(200).send({ result: 'ok', data: result });
     });
